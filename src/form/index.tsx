0a48e448f299c071bba99a435999bfff5ca2c774
tatsuzaki98 <tatsuzaki98@gmail.com>
2024-08-13 00:02:11 +0900

    SWRを用いてAPIの取得結果を保持する
    SWRのキーにsubmitされたフォーム入力値キャッシュを紐づけることで、
    フォーム入力値キャッシュが更新されるたびに郵便番号APIに問い合わせる。
    
    問い合わせたレスポンスはresponseCacheのdataに保持される。
    これをレンダリングすることで住所が画面上に表示される.

--- src/form/index.tsx
+++ src/form/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import useSWR from "swr";
 
 interface State {
   postalCode: string;
@@ -22,6 +23,7 @@ interface PostalcodeResponse {
 const fetcher = async ([url, postalCode]: [string, string]): Promise<PostalcodeResponse> => {
   const response = await fetch(`${url}?zipcode=${postalCode}`);
   const data = await response.json();
+  console.log(data);
   return data;
 }
 
@@ -29,6 +31,25 @@ const FormComponent = (): JSX.Element => {
   const [state, setState] = useState<State>({
     postalCode: "",
   });
+
+  const postalCodeCache = useSWR<string>(
+    '@postal-code',
+    null,
+  );
+
+  const responseCache = useSWR<PostalcodeResponse>(
+    ["https://zipcloud.ibsnet.co.jp/api/search", postalCodeCache.data],
+    fetcher,
+    {
+      /**
+       * SWRはデフォルトでフォーカス時・再接続時・マウント時に再検証を行い、fetcherを呼び出す
+       * 今回はフォームの入力値が変更された時のみ再検証を行えば良いので、以下のように設定する
+       */
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      revalidateOnMount: false,
+    }
+  );
 
   const handlers = {
     input: (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -39,16 +60,7 @@ const FormComponent = (): JSX.Element => {
     },
     submit: (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
-      fetcher([
-        "https://zipcloud.ibsnet.co.jp/api/search",
-        state.postalCode,
-      ])
-        .then((data) => {
-          console.log(data);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      postalCodeCache.mutate(state.postalCode);
     },
   };
 
@@ -56,6 +68,7 @@ const FormComponent = (): JSX.Element => {
     <form
       onSubmit={handlers.submit}
     >
+      {/* 入力 */}
       <label>
         郵便番号
       </label>
@@ -64,9 +77,25 @@ const FormComponent = (): JSX.Element => {
         value={state.postalCode}
         onChange={handlers.input}
       />
+
+      {/* 検索 */}
       <button type="submit">
         住所検索
       </button>
+
+      {/* ローディング */}
+      {responseCache.isValidating && <p>検索中...</p>}
+
+      {/* response */}
+      {responseCache?.data?.results?.map((result, index) => (
+        <div key={index}>
+          <span>
+            {result.address1}
+            {result.address2}
+            {result.address3}
+          </span>
+        </div>
+      ))}
     </form>
   );
 };
