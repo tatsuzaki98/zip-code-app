57cbb4cd6c8b6e1c861ebf5a4242faa32f463533
tatsuzaki98 <tatsuzaki98@gmail.com>
2024-08-12 17:40:53 +0900

    useStateフックでフォームの入力内容を保持する
    フォームに記入された郵便番号の情報をReact useStateフックで管理する。
    submitボタンを押すことで、フォームに記入された内容をconsoleに表示する。
    consoleは、ブラウザの開発者ツールから確認できる。

--- src/form/index.tsx
+++ src/form/index.tsx
@@ -1,8 +1,42 @@
+import { useState } from "react";
+
+interface State {
+  postalCode: string;
+}
+
 const FormComponent = (): JSX.Element => {
+  const [state, setState] = useState<State>({
+    postalCode: "",
+  });
+
+  const handlers = {
+    input: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setState({
+        ...state,
+        postalCode: e.target.value,
+      });
+    },
+    submit: (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log(state);
+    },
+  };
+
   return (
-    <form>
-      <input type="text" />
-      <button type="submit">Submit</button>
+    <form
+      onSubmit={handlers.submit}
+    >
+      <label>
+        郵便番号
+      </label>
+      <input
+        type="text"
+        value={state.postalCode}
+        onChange={handlers.input}
+      />
+      <button type="submit">
+        住所検索
+      </button>
     </form>
   );
 };
